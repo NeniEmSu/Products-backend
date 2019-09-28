fd3fde5941b391833b3bc5f337b480d3b42245ed
cryptodoctor <emmanuelneni@gmail.com>
2019-09-28 19:29:42 +0300

    initial commit

---
+++ routes/stuff.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
+
+const stuffCtrl = require('../controllers/stuff');
+
+
+
+router.post('/', auth, multer, stuffCtrl.createThing);
+
+router.get('/', auth, stuffCtrl.getAllThings);
+
+router.get('/:id', auth, stuffCtrl.getOneThing);
+
+router.put('/:id', auth, multer, stuffCtrl.updateOneThing);
+
+router.delete('/:id', auth, stuffCtrl.deleteOneThing);
+
+
+
+module.exports = router;
